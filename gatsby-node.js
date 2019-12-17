224a8a3b1467d6aa9f997981e375c37080c78641
Michal Piechowiak <misiek.piechowiak@gmail.com>
2019-12-17 17:35:26 +0100

    node with circular ref with unsupported types

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,46 @@
  */
 
 // You can delete this file if you're not using it
+exports.sourceNodes = ({ actions }) => {
+  const circularReference = {
+    fn: () => {},
+  }
+  circularReference.self = circularReference
+  const indirectCircular = {
+    down1: {
+      down2: {},
+    },
+  }
+  indirectCircular.down1.down2.deepCircular = indirectCircular
+
+  testNode = {
+    id: `id1`,
+    parent: null,
+    children: [],
+    unsupported: () => {},
+    inlineObject: {
+      field: `fieldOfFirstNode`,
+      re: /re/,
+    },
+    repeat: `foo`,
+    repeat2: `bar`,
+    repeat3: {
+      repeat3: {
+        test: () => {},
+        repeat: `bar`,
+      },
+    },
+    inlineArray: [1, 2, 3, Symbol(`test`)],
+    internal: {
+      type: `Test`,
+      contentDigest: `digest1`,
+      // owner: `test`,
+    },
+    circularReference,
+    deep: {
+      indirectCircular,
+    },
+  }
+
+  actions.createNode(testNode)
+}
